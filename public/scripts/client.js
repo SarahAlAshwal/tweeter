ee14e7582a09273495b4e5842a3a1ea8589da668
SarahAlAshwal <sa.alashwal@gmail.com>
2020-09-09 20:44:33 +0000

    adding form validation in the form event handler function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,30 +4,30 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
+// const data = [
+//   {
+//     "user": {
+//       "name": "Newton",
+//       "avatars": "https://i.imgur.com/73hZDYK.png"
+//       ,
+//       "handle": "@SirIsaac"
+//     },
+//     "content": {
+//       "text": "If I have seen further it is by standing on the shoulders of giants"
+//     },
+//     "created_at": 1461116232227
+//   },
+//   {
+//     "user": {
+//       "name": "Descartes",
+//       "avatars": "https://i.imgur.com/nlhLi3I.png",
+//       "handle": "@rd" },
+//     "content": {
+//       "text": "Je pense , donc je suis"
+//     },
+//     "created_at": 1461113959088
+//   }
+// ];
 
 const renderTweets = function(tweets) {
   // loops through tweets
@@ -75,10 +75,17 @@ $(document).ready(function(){
   //Form submission handeling
   $(".tweet-form").on('submit', function(evt) {
     evt.preventDefault();
-    console.log($('#tweet-text').serialize());
-    $.ajax('/tweets/', {method: 'POST', data: $('#tweet-text').serialize()} ).then(
-    
-    )
+    console.log($('#tweet-text').val());
+    if(! $('#tweet-text').val()){
+      alert("Sorry, you can't post an empty tweet");
+    } else if ($('#tweet-text').val().length > 140) {
+      alert('Sorry, your tweet is longer than 140 characters!!')
+    } else {
+      $.ajax('/tweets/', {method: 'POST', data: $('#tweet-text').serialize()} ).then(
+        console.log('inside post')
+        )
+    }
+   
   });
 } ) ;
 
